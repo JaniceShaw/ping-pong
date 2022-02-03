2bece8af93400b2d4523fe0f742fc077248bbd7c
Costas <chondrosconstantinos@gmail.com>
2022-02-03 21:59:05 +0100

    created new-job-structure, imported some tailwind components

--- frontend/src/Pages/NewJob/NewJob.jsx
+++ frontend/src/Pages/NewJob/NewJob.jsx
@@ -1,7 +1,51 @@
+import { useState } from 'react';
+import { SelectInput } from '../../Components/SelectInput.jsx/SelectInput';
+import { InputFile } from '../../Components/TailwindComp/InputFile';
+
 export const NewJob = () => {
+  const [title, setTitle] = useState('');
+  const [hourly_rate, setHourlyRate] = useState('');
+  const [max_reward, setMaxReward] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
     <>
-      <h1>New Job Page</h1>
+      <h1>Create a New Job Request!</h1>
+      <form className='flex flex-col justify-center items-center'>
+        <SelectInput />
+        <input
+          type='text'
+          placeholder='Title'
+          value={title}
+          className='outline-none border-2 '
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='hourly rate'
+          value={hourly_rate}
+          className='outline-none border-2 '
+          onChange={(e) => setHourlyRate(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='max reward'
+          value={max_reward}
+          className='outline-none border-2 '
+          onChange={(e) => setMaxReward(e.target.value)}
+        />
+        <SelectInput />
+        <textarea
+          value={description}
+          className='border-2'
+          cols='20'
+          onChange={(e) => setDescription(e.target.value)}
+          rows='5'></textarea>
+        <InputFile />
+        <InputFile />
+        <InputFile />
+        <input type='submit' value='submit changes' className='btn' />
+      </form>
     </>
   );
 };
