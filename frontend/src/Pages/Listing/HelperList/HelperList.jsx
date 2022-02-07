f7a7c4770dc16ef4b9a4e6ed713af0aec9936884
Giuseppe Di Guglielmo <gusbertus@gmail.com>
2022-02-07 23:40:37 +0100

    fetching jobs and helpers

--- frontend/src/Pages/Listing/HelperList/HelperList.jsx
+++ frontend/src/Pages/Listing/HelperList/HelperList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import { HelperCard } from '../../../Components/BigCards/HelperCard';
+import { getData } from '../../../Hooks/DataFetching';
 
 export const HelperListPage = () => {
+  const [helpersList, setHelpersList] = useState([]);
+
+  useState(() => {
+    getData('user/list/helpers/', setHelpersList);
+  }, []);
   return (
     <>
-      <HelperCard />
-      <HelperCard />
-      <HelperCard />
+      <div id='results_list' className='grid gap-4'>
+        {helpersList.map((helper, i) => (
+          <HelperCard key={i} helper={helper} />
+        ))}
+      </div>
     </>
   );
 };
