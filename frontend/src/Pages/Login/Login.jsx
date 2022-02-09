1e94e0168ad1e3c3d7206c9c1bba9556a366fb3f
Janice Shaw <hello@janiceshaw.co.uk>
2022-02-09 11:19:56 +0100

    changed login / logout on home page

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -19,6 +19,7 @@ export const LoginPage = () => {
         navigate('/');
     }
 
+
   const handleSignIn = (event) => {
     event.preventDefault();
 
--- frontend/src/Pages/Start/Start.jsx
+++ frontend/src/Pages/Start/Start.jsx
@@ -1,32 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import StudentsPicture from '../../Assets/UnDraw/students_undraw.png';
 
 export const StartPage = () => {
+     const [logedIn, setLogedIn] = useState(false);
+
+     let navigate = useNavigate();
+    const HomeLink = () => {
+        navigate('/');
+    }
+
+     const handleLogout = (event) => {
+      localStorage.clear();
+      setLogedIn(false)
+       HomeLink();
+  };
+
+     useEffect(() => {
+        if (localStorage.getItem('token')) {
+            setLogedIn(true)
+        }
+    },[setLogedIn]);
+
+     console.log(logedIn)
   return (
     <div className='Start w-full max-w-sm  text-center m-auto'>
         <h2 className='font-bold text-2xl mb-7 mt-3'>Where students and apprentices help you with anything.</h2>
         <img src={StudentsPicture} alt="here should be a Students drawing" className='mb-7' />
 
-        <button className='btn block m-auto mb-7 min-w-full'>
-            <Link to="/search">I need help </Link>
-        </button>
+        <Link to="/search">
+            <button className='btn block m-auto mb-7 min-w-full'>I need help</button>
+        </Link>
 
-        <button className='btn block m-auto mb-7 min-w-full'>
-            <Link to="/listing/jobs">I want to help</Link>
-        </button>
+        <Link to="/listing/jobs">
+            <button className='btn block m-auto mb-7 min-w-full'>I want to help</button>
+        </Link>
 
 
       <div className="loginButton mb-6">
-        <p>Have an account?</p>
-          <Link to="/login" className='underline underline-offset-2'>Log in</Link> or <Link to="/login" className='underline underline-offset-2'>Register</Link>
-      </div>
 
+          {logedIn?
+              (<Link to="/" className='underline underline-offset-2' onClick={handleLogout}>Logout</Link>)
+              :(<span>
+                  <p>Have an account?</p>
+                  <Link to="/login" className='underline underline-offset-2'>Log in</Link> or <Link to="/login" className='underline underline-offset-2'>Register</Link>
+              </span>)
+          }
+      </div>
 
       <Link to="/about" className='underline underline-offset-2'>
         <p>What is ping-pong?</p>
       </Link>
-
 
     </div>
   );
