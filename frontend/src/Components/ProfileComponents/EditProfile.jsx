af57070a31855202e2074bb7dce3476ce78c77ee
Costas <chondrosconstantinos@gmail.com>
2022-02-09 11:24:00 +0100

    most part of editing profile works

--- frontend/src/Components/ProfileComponents/EditProfile.jsx
+++ frontend/src/Components/ProfileComponents/EditProfile.jsx
@@ -1,25 +1,87 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { InputFile } from '../TailwindComp/InputFile';
+import { getData, patchData } from '../../Hooks/DataFetching';
+import { Category } from '../TailwindComp/CategorySelect';
 
 export const EditProfile = () => {
   const [username, setUsername] = useState('');
   const [first_name, setFirstName] = useState('');
   const [last_name, setLastName] = useState('');
-  const [birthday, setBirthday] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
+  const [profile_pic, setProfilePic] = useState('');
+  const [description, setDescription] = useState('');
+  const [helper_hourly_rate, setHourlyRate] = useState('');
+  const [categories, setCategories] = useState('');
+  const [helper_available, setAvailable] = useState('');
+  const [helper_verified, setVerified] = useState('');
+
+  const [error, setError] = useState(null);
 
   let navigate = useNavigate();
 
+  useEffect(() => {
+    getData(
+      'user/helper/me',
+      (setUsername,
+      setFirstName,
+      setLastName,
+      setEmail,
+      setPhone,
+      setStreet,
+      setCity,
+      setZip,
+      setProfilePic,
+      setDescription,
+      setHourlyRate,
+      setCategories,
+      setAvailable,
+      setVerified),
+      setError
+    );
+  }, []);
+
+  const form = {
+    username: username,
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    phone: phone,
+    street: street,
+    city: city,
+    zip: zip,
+    profile_pic: profile_pic,
+    description: description,
+    helper_hourly_rate: helper_hourly_rate,
+    categories: categories,
+    helper_available: helper_available,
+    helper_verified: helper_verified,
+  };
+
   const handleSubmitButton = (event) => {
     event.preventDefault();
-    navigate('/listing');
+    patchData('user/helper/me', form, setError);
   };
+
+  const handleImageUpload = (event) => {
+    event.preventDefault();
+    setProfilePic(event.target.files[0]);
+  };
+
   return (
     <>
       <form
         onSubmit={handleSubmitButton}
         className='flex flex-col items-center bg-slate-300 shadow-sm'>
-        <InputFile />
+        <input
+          className='w-1/2 border-2 border-black'
+          type='file'
+          name={profile_pic}
+          onChange={handleImageUpload}
+        />
         <input
           className='w-1/2 border-2 border-black'
           placeholder='username'
@@ -44,10 +106,69 @@ export const EditProfile = () => {
         />
         <input
           className='w-1/2 border-2 border-black'
-          placeholder='birthday'
+          placeholder='eMail'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='Phone'
+          type='tel'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='Street'
           type='text'
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
+          value={street}
+          onChange={(e) => setStreet(e.target.value)}
+        />
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='City'
+          type='text'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='Zip'
+          type='number'
+          value={zip}
+          onChange={(e) => setZip(e.target.value)}
+        />
+        <textarea
+          className='w-1/2 border-2 border-black'
+          placeholder='Description'
+          name=''
+          id=''
+          cols='20'
+          rows='3'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}></textarea>
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='Hourly Rate'
+          type='text'
+          value={helper_hourly_rate}
+          onChange={(e) => setHourlyRate(e.target.value)}
+        />
+        <Category />
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='Availability'
+          type='text'
+          value={helper_available}
+          onChange={(e) => setAvailable(e.target.value)}
+        />
+        <input
+          className='w-1/2 border-2 border-black'
+          placeholder='verified'
+          type='text'
+          name={helper_verified}
+          onChange={(e) => setVerified(e.target.value)}
         />
         <input className='w-1/2 border-2 border-black' type='submit' />
       </form>
--- frontend/src/Hooks/DataFetching.js
+++ frontend/src/Hooks/DataFetching.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 localStorage.setItem(
   'token',
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ0Njg5NTgyLCJqdGkiOiI0YjMzOWU2YWEzZTE0NDAzOGNlZWRlNDkzNjAwMzlkYSIsInVzZXJfaWQiOjR9.-XTRcalVZhHcupOq2nLojjWFfcXX7RpTqUhm3Gqbdac'
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ0Nzc2NTA4LCJqdGkiOiI1N2JkNjVjNzM5N2E0MGM5OGRmMjQ0NDY2MmJjYzdjYiIsInVzZXJfaWQiOjN9.59HE1i4xglWAOxh8jay7G_tBKyUpTv8BSZbDU0NO-9A'
 );
 const apiBaseURL = 'https://ping-pong.propulsion-learn.ch/backend/api/';
 const token = localStorage.getItem('token');
@@ -17,6 +17,7 @@ export const getData = (urlEnding, stateToUpdate, errorState) => {
     .get(`${apiBaseURL}${urlEnding}`, config)
     .then((response) => {
       stateToUpdate(response.data);
+      console.log(response.data.username);
     })
     .catch((err) => {
       errorState(err.response.data);
@@ -38,14 +39,14 @@ export const postData = (urlEnding, bodyObject, errorState) => {
     });
 };
 
-export const patchData = (urlEnding, id, bodyObject, errorState) => {
+export const patchData = (urlEnding, bodyObject, errorState) => {
   const formData = new FormData();
 
   for (let entry in bodyObject) {
     formData.append(entry, bodyObject[entry]);
   }
   axios
-    .patch(`${apiBaseURL}${urlEnding}${id}/`, formData, config)
+    .patch(`${apiBaseURL}${urlEnding}/`, formData, config)
     .then(() => {})
     .catch((err) => {
       errorState(err.response.data);
