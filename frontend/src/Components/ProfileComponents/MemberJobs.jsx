43b31f3b247e8000a732a83fc03162d8e7f22d17
Costas <chondrosconstantinos@gmail.com>
2022-02-11 16:30:15 +0100

    fetching job data

--- frontend/src/Components/ProfileComponents/MemberJobs.jsx
+++ frontend/src/Components/ProfileComponents/MemberJobs.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getData } from '../../Hooks/DataFetching';
 import { Smallcard } from '../SmallCards/SmallCard';
 
 export const MemberJobs = () => {
+  const [member_jobs, setMemberJobs] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getData('/job/list/review/member', setMemberJobs, setError);
+  }, []);
+
   return (
     <>
       <div className='jobs-container flex flex-col justify-around items-center'>
-        <Smallcard />
-        <Smallcard />
-        <Smallcard />
-        <Smallcard />
+        {/* {member_jobs.map((job, i) => (
+          <Smallcard key={i} job={job} />
+        ))} */}
       </div>
     </>
   );
