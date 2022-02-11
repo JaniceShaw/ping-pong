b5133dcfc5b6e88fe8da8a2ad335da4dee759ad9
Max Hagen <hagenmaximilian2020@gmail.com>
2022-02-11 17:43:40 +0100

    Fetching on Helper Profile and Member Profile, styling on profiles and started on top bar

--- frontend/src/Pages/Profile/HelperProfile/HelperProfile.jsx
+++ frontend/src/Pages/Profile/HelperProfile/HelperProfile.jsx
@@ -3,27 +3,26 @@ import { HelperInfo } from '../../../Components/ProfileComponents/HelperInfo';
 import { HelperJobs } from '../../../Components/ProfileComponents/HelperJobs';
 import { useParams } from 'react-router-dom';
 
+
 export const HelperProfilePage = () => {
   const [show_jobs, setShowJobs] = useState(false);
   const { profileID } = useParams();
   
-  console.log(profileID)
-
   return (
     <>
-      <div className='section flex  justify-evenly '>
+      <div className='pt-3 section flex w-screen -mx-4 border-b border-b-1 border-primary grow-1 justify-center'>
         <button
           onClick={() => setShowJobs(false)}
-          className='border-2 border-black w-full'>
+          className='border-primary border-r-1 border-r text-2xl content-center'>
           Profile
         </button>
         <button
           onClick={() => setShowJobs(true)}
-          className='border-2 border-black w-full'>
+          className='border-primary border-l-1 border-l text-2xl content-center'>
           Jobs
         </button>
       </div>
-      {!show_jobs ? <HelperInfo /> : <HelperJobs />}
+      {!show_jobs ? <HelperInfo profileID={profileID} /> : <HelperJobs profileID={profileID} />}
     </>
   );
 };
