176d6550ccf0b38b7d3d371e6012300acbd61f84
Costas <chondrosconstantinos@gmail.com>
2022-02-12 09:50:50 +0100

    made register work

--- frontend/src/Pages/Register/Validation/Validation.jsx
+++ frontend/src/Pages/Register/Validation/Validation.jsx
@@ -1,88 +1,160 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export const Validation = () => {
-
   const [password, setPassword] = useState('');
+  const [password_repeat, setPasswordRepeat] = useState('');
   const [email, setEmail] = useState('');
   const [code, setCode] = useState('');
-  // const [user, setUser] = useState('');
-  // // login errors
-  // const [error, setError] = useState('');
+  const [username, setUsername] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
 
-  // for react router to change page
   let navigate = useNavigate();
-
-  // const dispatch = useDispatch();
 
   const handleSignIn = (event) => {
     event.preventDefault();
-    // GetUserToken(email, password, setError);
-    // GetUserData(setUser, setError);
   };
-
-  // useEffect(() => {
-  //   // dispatch(
-  //   //   signInAction({
-  //   //     user: user.first_name,
-  //   //     user_id: user.id,
-  //   //   })
-  //   // );
-  //   if (user.id) {
-  //     navigate('/profile');
-  //   }
-  // }, []);
 
   const handlePasswordInput = (event) => {
     setPassword(event.target.value);
   };
 
-  const handleEmailInput = (event) => {
-    setEmail(event.target.value);
-  };
-
   const handleCodeInput = (event) => {
     setCode(event.target.value);
   };
-    return (
-      <>
-        <h1>Thanks for registering!</h1>
-        <br/>
-        <p>Please check your email for your verification code and login with it below.</p>
-        <br/>
-        <div className="registerForm">
-          <form action="">
-            <input
-              className="field-input"
-              type="email"
-              placeholder="Your E-Mail"
-              value={ email }
-              onChange={handleEmailInput}
-            />
-            <input
-              className="field-input"
-              type="password"
-              placeholder="Your Password"
-              value={ password }
-              onChange={handlePasswordInput}
-            />
-            <input
-              className="field-input"
-              type="text"
-              placeholder="E-Mail code"
-              value={ code }
-              onChange={handleCodeInput}
-            />
-            <input
-              type="submit"
-              className="field-submit"
-              value="Submit"
-              onClick={handleSignIn}
-            />
-          </form>
-        </div>
-      </>
-    );
-  };
-  
+  return (
+    <>
+      <h1 className='text-xl mt-3 pb-6 font-bold'>Thanks for registering!</h1>
+      <br />
+      <p className='text-xl mt-3 pb-6 font-bold'>
+        Please check your email for your verification code and login with it
+        below.
+      </p>
+      <br />
+      <div className='loginForm w-full max-w-sm  text-center m-auto'>
+        <form className='flex justify-center flex-col mt-5'>
+          <input
+            className='field-input-login peer'
+            type='email'
+            required
+            placeholder='e-Mail'
+            onChange={(e) => setEmail(e.target.value)}
+            id='email'
+          />
+          <label htmlFor='email' className='login-label'>
+            Email address
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {email['email']}
+          </div>
+
+          <input
+            className='field-input-login peer'
+            type='username'
+            required
+            placeholder='username'
+            onChange={(e) => setUsername(e.target.value)}
+            id='username'
+          />
+          <label htmlFor='username' className='login-label'>
+            Username
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {username['username']}
+          </div>
+
+          <input
+            className='field-input-login peer'
+            type='username'
+            required
+            placeholder='username'
+            onChange={(e) => setUsername(e.target.value)}
+            id='username'
+          />
+          <label htmlFor='username' className='login-label'>
+            Username
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {username['username']}
+          </div>
+
+          <input
+            className='field-input-login peer'
+            type='password_repeat'
+            required
+            placeholder='password_repeat'
+            onChange={(e) => setUsername(e.target.value)}
+            id='password_repeat'
+          />
+          <label htmlFor='password_repeat' className='login-label'>
+            Password Repeat
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {username['username']}
+          </div>
+
+          <input
+            className='field-input-login peer'
+            type='type'
+            required
+            placeholder='type'
+            onChange={(e) => setUsername(e.target.value)}
+            id='type'
+          />
+          <label htmlFor='type' className='login-label'>
+            Username
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {type['type']}
+          </div>
+
+          <input
+            className='field-input-login peer'
+            type='firstname'
+            required
+            placeholder='firstname'
+            onChange={(e) => setFirstName(e.target.value)}
+            id='firstname'
+          />
+          <label htmlFor='firstname' className='login-label'>
+            Username
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {first_name['firstname']}
+          </div>
+
+          <input
+            className='field-input-login peer'
+            type='lastname'
+            required
+            placeholder='lastname'
+            onChange={(e) => setLastName(e.target.value)}
+            id='lastname'
+          />
+          <label htmlFor='lastname' className='login-label'>
+            Lastname
+          </label>
+
+          <div className='text-xs text-red-500 h-3 text-left' id='emailHelp'>
+            {last_name['lastname']}
+          </div>
+
+          <input
+            type='submit'
+            className='btn'
+            value='Submit'
+            onClick={handleSignIn}
+          />
+        </form>
+      </div>
+    </>
+  );
+};
