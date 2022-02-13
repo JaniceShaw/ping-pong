e88ea261e10e30fd831601e0bc9aaa57f6cd76b6
Giuseppe Di Guglielmo <gusbertus@gmail.com>
2022-02-13 20:40:37 +0100

    styled error message

--- frontend/src/Pages/Error/Error.jsx
+++ frontend/src/Pages/Error/Error.jsx
@@ -1,3 +1,18 @@
+import ErrorImage from '../../Assets/UnDraw/undraw_cancel_re_ctke.svg';
+import { Link } from 'react-router-dom';
+
 export const ErrorPage = () => {
-  return <h1>Error Page</h1>;
+  return (
+    <>
+      <div className='flex-col flex justify-between h-full max-h-screen'>
+        <h1 className=' font-bold text-red-500 text-3xl pt-8 uppercase text-center'>
+          nothing to see here!
+        </h1>
+        <img src={ErrorImage} alt='error page' />
+        <Link className='btn' to='/'>
+          back to home
+        </Link>
+      </div>
+    </>
+  );
 };
