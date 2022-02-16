afaab97839537adfc29a9bd76a0c36c84e11aa7f
Janice Shaw <hello@janiceshaw.co.uk>
2022-02-16 13:41:37 +0100

    cleaned up pages

--- frontend/src/Pages/FAQ/FAQ.jsx
+++ frontend/src/Pages/FAQ/FAQ.jsx
@@ -4,42 +4,44 @@ import { Link } from 'react-router-dom';
 export const FAQPage = () => {
   return (
     <>
-        <h1>Frequently Asked Questions</h1>
+        <h1 className='text-2xl mt-4 mb-4 font-bold'>Frequently Asked Questions</h1>
         <br/>
-        <p>Question 1:</p>
+        <p className='font-bold'>Question 1:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 2:</p>
+        <p className='font-bold'>Question 2:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 3:</p>
+         <p className='font-bold'>Question 3:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 4:</p>
+         <p className='font-bold'>Question 4:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 5:</p>
+         <p className='font-bold'>Question 5:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 6:</p>
+         <p className='font-bold'>Question 6:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 7:</p>
+         <p className='font-bold'>Question 7:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 8:</p>
+         <p className='font-bold'>Question 8:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 9:</p>
+         <p className='font-bold'>Question 9:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Question 10:</p>
+         <p className='font-bold'>Question 10:</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         <br/>
-        <p>Any other questions?</p>
+        <h2 className='text-xl mt-4 mb-4 font-bold'>Any other questions?</h2>
+
         <Link to='/support'>Contact Support</Link>
         <br/>
         <Link to='/'>Return to Start</Link>
+
         <div>
             <Link to='/register'>Register</Link>
             <p>or</p>
